a6bc83389dc595c2feca4957f6bfbb9ea5791756
Várnai Dávid <shaxvideos@gmail.com>
2022-05-27 23:09:17 +0200

    Initial Favorites

--- pages/tracker/favorites.tsx
+++ pages/tracker/favorites.tsx
@@ -6,9 +6,9 @@ import Router, { useRouter } from "next/router";
 import toast from "react-hot-toast";
 import { useUser } from "@supabase/supabase-auth-helpers/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { isValidElement, useEffect, useState } from "react";
 import styles from "../../styles/Tracker.module.scss";
-import { CoinList } from "../../lib/CoinGecko";
+import { CoinList, SingleCoin } from "../../lib/CoinGecko";
 import { CryptoState } from "../../lib/MainContext";
 
 export default function SignUp() {
@@ -16,23 +16,16 @@ export default function SignUp() {
   const router = useRouter();
 
   const { isLoading, user } = useUser();
-  const [favorites, setFavorites] = useState([]);
 
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState("");
+  const [favorites, setFavorites] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(15);
 
+  const [coins, setCoins] = useState([]);
+
   const [total, setTotal] = useState([]);
   const [totalLoading, setTotalLoading] = useState(true);
-
-  const fetchCoins = async () => {
-    setLoading(true);
-    const { data } = await axios.get(CoinList(currency));
-    setCoins(data);
-    setLoading(false);
-  };
 
   const fetchTotal = async () => {
     setTotalLoading(true);
@@ -41,40 +34,32 @@ export default function SignUp() {
     setTotalLoading(false);
   };
 
-  useEffect(() => {
-    fetchCoins();
-  }, [currency]);
-
-  useEffect(() => {
-    fetchTotal();
+  const fetchFavorites = () => {
     if (user && !isLoading) {
       const response = supabaseClient
         .from<any>("favorites")
         .select("favorites")
         .match({ id: user.id });
       response.then((res) => {
+        res.data.at(0)["favorites"].forEach(async (id) => {
+          const { data } = await axios.get(SingleCoin(id));
+          setCoins((prevArray) => [...prevArray, data]);
+        });
         setFavorites(res.data.at(0)["favorites"]);
       });
     }
-  }, [user, isLoading]);
-
-  const filteredCoins = coins.filter(
-    (coin) =>
-      coin.name.toLowerCase().includes(search.toLowerCase()) ||
-      coin.symbol.toLowerCase().includes(search.toLowerCase())
-  );
+  };
 
   useEffect(() => {
-    if (user && !isLoading && favorites.length != 0) {
-      const response = supabaseClient
-        .from<any>("favorites")
-        .update({ favorites: favorites })
-        .match({ id: user.id })
-        .then((res) => {
-          return; /**/
-        });
+    fetchTotal();
+    if (favorites.length === 0) {
+      fetchFavorites();
     }
-  }, [favorites]);
+
+    /*  */
+    /* NOT WORKING */
+    /*  */
+  }, [user, isLoading]);
 
   return (
     <div>
@@ -162,15 +147,6 @@ export default function SignUp() {
               </div>
             </div>
           )}
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-primary mt-10 mb-5 px-3 py-5 w-full"
-            onChange={(e) => {
-              setSearch(e.target.value);
-              setPage(1);
-            }}
-          />
           <div className="flex my-6">
             <Link href="/tracker/coins">
               <button className="btn btn-ghost">Cryptocurrencies</button>
@@ -205,7 +181,7 @@ export default function SignUp() {
                 </>
               )
             ) : (
-              filteredCoins
+              coins
                 .slice((page - 1) * perPage, (page - 1) * perPage + perPage)
                 .map((coin) => {
                   const profit = coin.price_change_percentage_24h > 0;
@@ -269,7 +245,7 @@ export default function SignUp() {
                         </div>
                         <div className={`${styles.name}`}>
                           <Image
-                            src={coin.image}
+                            src={coin.image.small}
                             height={30}
                             width={30}
                             objectFit="fill"
@@ -281,21 +257,25 @@ export default function SignUp() {
                         </div>
                         <div className={`${styles.price}`}>
                           {symbol}
-                          {coin.current_price.toLocaleString()}
+                          {coin.market_data.current_price[
+                            currency.toLowerCase()
+                          ].toLocaleString()}
                         </div>
                         <div className={`${styles.day}`}>
-                          {coin.price_change_percentage_24h >= 0 ? (
+                          {coin.market_data.price_change_percentage_24h >= 0 ? (
                             <div className="bg-success bg-opacity-20 w-max p-1 rounded-md">
                               <i className="fas fa-angle-up text-success" />{" "}
                               <span className="text-success font-bold tracking-wide">
-                                {coin.price_change_percentage_24h.toFixed(2)}
+                                {coin.market_data.price_change_percentage_24h.toFixed(
+                                  2
+                                )}
                               </span>
                             </div>
                           ) : (
                             <div className="bg-error bg-opacity-20 w-max p-1 rounded-md">
                               <i className="fas fa-angle-down text-error" />{" "}
                               <span className="text-error font-bold tracking-wide">
-                                {coin.price_change_percentage_24h
+                                {coin.market_data.price_change_percentage_24h
                                   .toFixed(2)
                                   .slice(1)}
                               </span>
@@ -304,7 +284,9 @@ export default function SignUp() {
                         </div>
                         <div className={`${styles.marketCap}`}>
                           {symbol}
-                          {coin.market_cap.toLocaleString()}
+                          {coin.market_data.market_cap[
+                            currency.toLowerCase()
+                          ].toLocaleString()}
                         </div>
                       </div>
                       {/* </Link> */}
@@ -330,9 +312,7 @@ export default function SignUp() {
             <button className="btn btn-primary no-animation">{page}</button>
             <button
               className="btn btn-primary"
-              disabled={
-                page * perPage - filteredCoins.length >= 0 ? true : false
-              }
+              disabled={page * perPage - favorites.length >= 0 ? true : false}
               onClick={() => setPage(page + 1)}
             >
               <i className="fas fa-angle-right" />
